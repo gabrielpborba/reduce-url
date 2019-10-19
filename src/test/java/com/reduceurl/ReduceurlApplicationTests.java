fa5a19453f64a4b578bc6a6f1a74c30dff751832
gabriel <pastregabriel@hotmail.com>
2019-10-18 22:12:11 -0300

    initial commit

---
+++ src/test/java/com/reduceurl/ReduceurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reduceurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReduceurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
