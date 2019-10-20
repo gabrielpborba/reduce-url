808ec83f86c2a858567a39f563cc4c8bc5e9ed8c
gabrielpborba <pastregabriel@hotmail.com>
2019-10-20 02:00:56 -0300

    rabbitmq producer and consumer, fix configs

--- src/main/java/com/reduceurl/controller/ReduceUrlController.java
+++ src/main/java/com/reduceurl/controller/ReduceUrlController.java
@@ -1,20 +1,27 @@
 package com.reduceurl.controller;
 
+import com.reduceurl.dao.UrlRepository;
 import com.reduceurl.json.UrlJson;
+import com.reduceurl.model.Url;
 import com.reduceurl.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class ReduceUrlController {
 
     @Autowired
     private UrlService urlService;
+
+    @Autowired
+    private UrlRepository urlRepository;
 
     @PostMapping("/reduce")
     @ResponseBody
@@ -30,6 +37,16 @@ public class ReduceUrlController {
         httpServletResponse.sendRedirect(fullUrl);
 
     }
+
+    @RequestMapping("/all")
+    public @ResponseBody
+    ResponseEntity<Object> findAll()  {
+       List<Url> list =  urlRepository.findAll();
+        return new ResponseEntity<Object>(list, HttpStatus.OK);
+
+    }
+
+
 
     @RequestMapping("/redirect2/{url}")
     public RedirectView  redirect2(@PathVariable String url) {
--- src/main/java/com/reduceurl/service/UrlService.java
+++ src/main/java/com/reduceurl/service/UrlService.java
@@ -1,8 +1,21 @@
 package com.reduceurl.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.hash.Hashing;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.reduceurl.config.RabbitConfig;
+import com.reduceurl.consumer.MessageConsumer;
 import com.reduceurl.dao.UrlRepository;
 import com.reduceurl.model.Url;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +27,20 @@ public class UrlService {
     @Autowired
     private UrlRepository urlRepository;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    static final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @Autowired
+    public UrlService(RabbitTemplate rabbitTemplate, ObjectMapper objectMapper) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+
     public String createShortUrl(String url){
         String shortUrl = Hashing.murmur3_32().hashString(url, Charset.defaultCharset()).toString();
 
@@ -21,11 +48,11 @@ public class UrlService {
         newUrl.setFullUrl(url);
         newUrl.setShortUrl(shortUrl);
 
-        //se fila cair retorna a url inteira
-
-
-        //colocar na fila
-        urlRepository.save(newUrl);
+        try {
+            this.rabbitTemplate.convertAndSend(RabbitConfig.QUEUE_URL, newUrl);
+        }catch (Exception e){
+            logger.error("RABBIT MQ IS DOWN!");
+        }
 
         return shortUrl;
 
